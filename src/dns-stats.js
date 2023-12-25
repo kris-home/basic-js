b9afaf15c8a08acf41a4bfc939026784c75699e6
Kristina Homenkova <kristina.khomenkova@gmail.com>
2023-12-25 20:38:37 +0300

    feat: completed dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  domains.forEach((items) => {
+    const arr = items.split(".").reverse();
+    let dns = "";
+    arr.forEach((item) => {
+      dns += `.${item}`;
+      result[dns] = result[dns] ? result[dns] + 1 : 1;
+    })
+  });
+  return result;
 }
 
 module.exports = {
